1cbdbeb8c44406e4472ac8b966a47f2ce7a553c1
Edilio Gallardo <edilio@swagup.com>
2019-05-13 19:22:37 -0400

    first sample for posting a text to an object while mentionion a user

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # salesforce-chatter
 Python Library for Salesforce Chatter
+
+# Chatter API
+https://developer.salesforce.com/docs/atlas.en-us.chatterapi.meta/chatterapi/intro_what_is_chatter_connect.htm
+
+# How to run a the sample
+- `sudo gem install dotenv`
+- `cp sample-env .env`
+- fill out the params in .env file
+- modify `post_text_with_mention` for `subject_id`, `text`, `mention_to` to use your objects
+- run `dotenv ./sample.py`
---
+++ sample.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import os
+
+from chatter import Chatter, ChatterAuth
+
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+
+def post_text_with_mention(subject_id="0017A00000TW5tbQAD", text="Mockups generated successfully!!!",
+                           mention_to="0057A0000024xF7QAI"):
+    auth = ChatterAuth(username=USERNAME, password=PASSWORD, client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+    chatter = Chatter(auth=auth)
+
+    feed_item = {
+        "body": {
+            "messageSegments": [
+                {
+                    "type": "Text",
+                    "text": text
+                },
+                {
+                    "type": "Mention",
+                    "id": mention_to
+                }
+            ]
+        },
+        "feedElementType": "FeedItem",
+        "subjectId": subject_id
+    }
+    return chatter.feed_elements(feed_item)
+
+
+def main():
+    print(post_text_with_mention())
+
+
+if __name__ == '__main__':
+    main()
