c7252e1dbd70ba55220019bc7e899de543797d53
Edilio Gallardo <edilio@swagup.com>
2021-09-14 12:45:07 -0400

    upgrade setup.py for upgrade requests to requests==2.26.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='salesforce-chatter',
-    version='0.1',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Python Library for Salesforce Chatter',
     long_description=open('README.md').read(),
-    install_requires=['requests==2.21.0'],
+    install_requires=['requests==2.26.0'],
     url='https://github.com/swagup-com/salesforce-chatter',
     author='Dak Gonzalez',
     author_email='dak@swagup.com'
